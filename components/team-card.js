2a52823ddd438da359e03e9ab52a3616b195e0c1
abdullah-ch <abdullahchaudharry@gmail.com>
2021-06-29 19:15:22 +0500

    fixed responsiveness issues

--- components/team-card.js
+++ components/team-card.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { makeStyles, Card, Typography, Grid } from '@material-ui/core';
 
-const styles = makeStyles(() => ({
+const styles = makeStyles((theme) => ({
     images: {
         width: '270px !important',
         height: '270px !important',
         objectFit: 'cover !important',
         filter: 'grayscale(100%)',
+    },
+
+    cardSize: {
+        [theme.breakpoints.down(810)]: {
+            marginTop: '1rem',
+        },
     },
 }));
 
@@ -16,36 +22,13 @@ const TeamCard = ({ data }) => {
 
     return (
         <>
-            <Card>
+            <Card classes={{ root: classes.cardSize }}>
                 <img className={classes.images} src={image} alt={name} />
                 <div className='detail_TM'>
                     <h5>{name}</h5>
                     <h6>{designation}</h6>
                 </div>
             </Card>
-            {/*
-             <div className='singleTM'>
-                <div className='tm_img'>
-                    <img src={image} alt={name} />
-                    <div className='tm_overlay'>
-                        <div className='team_social'>
-                            {socials.map(({ name, url }, index) => (
-                                <a key={index} href={url}>
-                                    <span>{name}</span>
-                                </a>
-                            ))}
-                        </div>
-                        <a href={url} className='common_btn'>
-                            <span>learn more</span>
-                        </a>
-                    </div>
-                </div>
-                <div className='detail_TM'>
-                    <h5>{name}</h5>
-                    <h6>{designation}</h6>
-                </div>
-            </div> 
-            */}
         </>
     );
 };
